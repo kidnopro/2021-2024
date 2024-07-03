2c75671ac6b3f1ab2a9a50666efd6daeed1c0ae9
kidnopro <hoanganhfullstack@gmail.com>
2024-07-03 08:19:57 +0700

    Add initial code with auto-play music feature

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ var autoRotate = true;
 var rotateSpeed = -60; 
 var imgWidth = 120; 
 var imgHeight = 170; 
-
 var bgMusicURL = 'https://www.youtube.com/watch?v=Trjrj_fQnIM';
 var bgMusicControls = true; 
-
-
 
 setTimeout(init, 1500);
 
@@ -16,7 +13,6 @@ var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
 var aEle = [...aImg, ...aVid]; 
-
 
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
@@ -54,12 +50,11 @@ if (autoRotate) {
 
 if (bgMusicURL) {
   document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<audio id="backgroundMusic" src="${bgMusicURL}" ${bgMusicControls ? 'controls' : ''} autoplay loop>
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
 }
-
 
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
@@ -106,31 +101,22 @@ document.onmousewheel = function(e) {
   init(1);
 };
 
-
-
-
-
-
 var canvas = document.getElementById("canvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 
 var gl = canvas.getContext('webgl');
 if(!gl){
   console.error("Unable to initialize WebGL.");
 }
 
-
 var time = 0.0;
-
-
 
 var vertexSource = `
 attribute vec2 position;
 void main() {
-	gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
@@ -154,111 +140,110 @@ float radius = 0.008;
 //https://www.shadertoy.com/view/MlKcDD
 //Signed distance to a quadratic bezier
 float sdBezier(vec2 pos, vec2 A, vec2 B, vec2 C){    
-	vec2 a = B - A;
-	vec2 b = A - 2.0*B + C;
-	vec2 c = a * 2.0;
-	vec2 d = A - pos;
+  vec2 a = B - A;
+  vec2 b = A - 2.0*B + C;
+  vec2 c = a * 2.0;
+  vec2 d = A - pos;
 
-	float kk = 1.0 / dot(b,b);
-	float kx = kk * dot(a,b);
-	float ky = kk * (2.0*dot(a,a)+dot(d,b)) / 3.0;
-	float kz = kk * dot(d,a);      
+  float kk = 1.0 / dot(b,b);
+  float kx = kk * dot(a,b);
+  float ky = kk * (2.0*dot(a,a)+dot(d,b)) / 3.0;
+  float kz = kk * dot(d,a);      
 
-	float res = 0.0;
+  float res = 0.0;
 
-	float p = ky - kx*kx;
-	float p3 = p*p*p;
-	float q = kx*(2.0*kx*kx - 3.0*ky) + kz;
-	float h = q*q + 4.0*p3;
+  float p = ky - kx*kx;
+  float p3 = p*p*p;
+  float q = kx*(2.0*kx*kx - 3.0*ky) + kz;
+  float h = q*q + 4.0*p3;
 
-	if(h >= 0.0){ 
-		h = sqrt(h);
-		vec2 x = (vec2(h, -h) - q) / 2.0;
-		vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
-		float t = uv.x + uv.y - kx;
-		t = clamp( t, 0.0, 1.0 );
+  if(h >= 0.0){ 
+    h = sqrt(h);
+    vec2 x = (vec2(h, -h) - q) / 2.0;
+    vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
+    float t = uv.x + uv.y - kx;
+    t = clamp( t, 0.0, 1.0 );
 
-		// 1 root
-		vec2 qos = d + (c + b*t)*t;
-		res = length(qos);
-	}else{
-		float z = sqrt(-p);
-		float v = acos( q/(p*z*2.0) ) / 3.0;
-		float m = cos(v);
-		float n = sin(v)*1.732050808;
-		vec3 t = vec3(m + m, -n - m, n - m) * z - kx;
-		t = clamp( t, 0.0, 1.0 );
+    // 1 root
+    vec2 qos = d + (c + b*t)*t;
+    res = length(qos);
+  }else{
+    float z = sqrt(-p);
+    float v = acos( q/(p*z*2.0) ) / 3.0;
+    float m = cos(v);
+    float n = sin(v)*1.732050808;
+    vec3 t = vec3(m + m, -n - m, n - m) * z - kx;
+    t = clamp( t, 0.0, 1.0 );
 
-		// 3 roots
-		vec2 qos = d + (c + b*t.x)*t.x;
-		float dis = dot(qos,qos);
+    // 3 roots
+    vec2 qos = d + (c + b*t.x)*t.x;
+    float dis = dot(qos,qos);
         
-		res = dis;
+    res = dis;
 
-		qos = d + (c + b*t.y)*t.y;
-		dis = dot(qos,qos);
-		res = min(res,dis);
-		
-		qos = d + (c + b*t.z)*t.z;
-		dis = dot(qos,qos);
-		res = min(res,dis);
-
-		res = sqrt( res );
-	}
+    qos = d + (c + b*t.y)*t.y;
+    dis = dot(qos,qos);
+    res = min(res,dis);
     
-	return res;
-}
+    qos = d + (c + b*t.z)*t.z;
+    dis = dot(qos,qos);
+    res = min(res,dis);
 
+    res = sqrt( res );
+  }
+    
+  return res;
+}
 
 //http://mathworld.wolfram.com/HeartCurve.html
 vec2 getHeartPosition(float t){
-	return vec2(16.0 * sin(t) * sin(t) * sin(t),
-							-(13.0 * cos(t) - 5.0 * cos(2.0*t)
-							- 2.0 * cos(3.0*t) - cos(4.0*t)));
+  return vec2(16.0 * sin(t) * sin(t) * sin(t),
+              -(13.0 * cos(t) - 5.0 * cos(2.0*t)
+              - 2.0 * cos(3.0*t) - cos(4.0*t)));
 }
 
 //https://www.shadertoy.com/view/3s3GDn
 float getGlow(float dist, float radius, float intensity){
-	return pow(radius/dist, intensity);
+  return pow(radius/dist, intensity);
 }
 
 float getSegment(float t, vec2 pos, float offset, float scale){
-	for(int i = 0; i < POINT_COUNT; i++){
-		points[i] = getHeartPosition(offset + float(i)*len + fract(speed * t) * 6.28);
-	}
+  for(int i = 0; i < POINT_COUNT; i++){
+    points[i] = getHeartPosition(offset + float(i)*len + fract(speed * t) * 6.28);
+  }
     
-	vec2 c = (points[0] + points[1]) / 2.0;
-	vec2 c_prev;
-	float dist = 10000.0;
+  vec2 c = (points[0] + points[1]) / 2.0;
+  vec2 c_prev;
+  float dist = 10000.0;
     
-	for(int i = 0; i < POINT_COUNT-1; i++){
-		//https://tinyurl.com/y2htbwkm
-		c_prev = c;
-		c = (points[i] + points[i+1]) / 2.0;
-		dist = min(dist, sdBezier(pos, scale * c_prev, scale * points[i], scale * c));
-	}
-	return max(0.0, dist);
+  for(int i = 0; i < POINT_COUNT-1; i++){
+    //https://tinyurl.com/y2htbwkm
+    c_prev = c;
+    c = (points[i] + points[i+1]) / 2.0;
+    dist = min(dist, sdBezier(pos, scale * c_prev, scale * points[i], scale * c));
+  }
+  return max(0.0, dist);
 }
 
 void main(){
-	vec2 uv = gl_FragCoord.xy/resolution.xy;
-	float widthHeightRatio = resolution.x/resolution.y;
-	vec2 centre = vec2(0.5, 0.5);
-	vec2 pos = centre - uv;
-	pos.y /= widthHeightRatio;
-	//Shift upwards to centre heart
-	pos.y += 0.02;
-	float scale = 0.000015 * height;
-	
-	float t = time;
+  vec2 uv = gl_FragCoord.xy/resolution.xy;
+  float widthHeightRatio = resolution.x/resolution.y;
+  vec2 centre = vec2(0.5, 0.5);
+  vec2 pos = centre - uv;
+  pos.y /= widthHeightRatio;
+  //Shift upwards to centre heart
+  pos.y += 0.02;
+  float scale = 0.000015 * height;
+  
+  float t = time;
     
-	//Get first segment
+  //Get first segment
   float dist = getSegment(t, pos, 0.0, scale);
   float glow = getGlow(dist, radius, intensity);
   
   vec3 col = vec3(0.0);
 
-	//White core
+  //White core
   col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
   //Pink glow
   col += glow * vec3(1.0,0.05,0.3);
@@ -272,14 +257,14 @@ void main(){
   //Blue glow
   col += glow * vec3(0.1,0.4,1.0);
         
-	//Tone mapping
-	col = 1.0 - exp(-col);
+  //Tone mapping
+  col = 1.0 - exp(-col);
 
-	//Gamma
-	col = pow(col, vec3(0.4545));
+  //Gamma
+  col = pow(col, vec3(0.4545));
 
-	//Output to screen
- 	gl_FragColor = vec4(col,1.0);
+  //Output to screen
+   gl_FragColor = vec4(col,1.0);
 }
 `;
 
@@ -288,19 +273,17 @@ window.addEventListener('resize', onWindowResize, false);
 function onWindowResize(){
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
-	gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.viewport(0, 0, canvas.width, canvas.height);
   gl.uniform1f(widthHandle, window.innerWidth);
   gl.uniform1f(heightHandle, window.innerHeight);
 }
-
-
 
 function compileShader(shaderSource, shaderType){
   var shader = gl.createShader(shaderType);
   gl.shaderSource(shader, shaderSource);
   gl.compileShader(shader);
   if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-  	throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
   }
   return shader;
 }
@@ -308,7 +291,7 @@ function compileShader(shaderSource, shaderType){
 function getAttribLocation(program, name) {
   var attributeLocation = gl.getAttribLocation(program, name);
   if (attributeLocation === -1) {
-  	throw 'Cannot find attribute ' + name + '.';
+    throw 'Cannot find attribute ' + name + '.';
   }
   return attributeLocation;
 }
@@ -316,16 +299,13 @@ function getAttribLocation(program, name) {
 function getUniformLocation(program, name) {
   var attributeLocation = gl.getUniformLocation(program, name);
   if (attributeLocation === -1) {
-  	throw 'Cannot find uniform ' + name + '.';
+    throw 'Cannot find uniform ' + name + '.';
   }
   return attributeLocation;
 }
 
-
-
 var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
 var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
-
 
 var program = gl.createProgram();
 gl.attachShader(program, vertexShader);
@@ -334,7 +314,6 @@ gl.linkProgram(program);
 
 gl.useProgram(program);
 
-
 var vertexData = new Float32Array([
   -1.0,  1.0, 	
   -1.0, -1.0, 	
@@ -342,11 +321,9 @@ var vertexData = new Float32Array([
    1.0, -1.0, 	
 ]);
 
-
 var vertexDataBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
-
 
 var positionHandle = getAttribLocation(program, 'position');
 
@@ -357,7 +334,7 @@ gl.vertexAttribPointer(positionHandle,
   false, 		
   2 * 4, 		
   0 				
-  );
+);
 
 var timeHandle = getUniformLocation(program, 'time');
 var widthHandle = getUniformLocation(program, 'width');
@@ -370,18 +347,16 @@ var lastFrame = Date.now();
 var thisFrame;
 
 function draw(){
-	
-  
-	thisFrame = Date.now();
+  thisFrame = Date.now();
   time += (thisFrame - lastFrame)/1000;	
-	lastFrame = thisFrame;
+  lastFrame = thisFrame;
 
-	
   gl.uniform1f(timeHandle, time);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   requestAnimationFrame(draw);
 }
+
 window.onload = function() {
   var backgroundMusic = document.getElementById("backgroundMusic");
   backgroundMusic.play();
